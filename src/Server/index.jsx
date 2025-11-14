51fc960b11ceb252bf138936cc49ad10fd0ef90d
danielfeliperu.jcol <danielfeliperuiz.jcol2@masorange.es>
2025-11-14 06:48:56 -0500

    Feat: agrega .env y modifica package.json

--- src/Server/index.jsx
+++ src/Server/index.jsx
@@ -1,5 +1,5 @@
 const urlAutentication = import.meta.env.VITE_REACT_APP_API_AUTENTICATION;
-const GateWay = import.meta.env.VITE_REACT_APP_API_GATEWAY
+const GateWay = import.meta.env.VITE_REACT_APP_API_GATEWAY;
 const urlIceberg = import.meta.env.VITE_URL_ICEBERG;
 
 export { urlAutentication, urlIceberg, GateWay };
