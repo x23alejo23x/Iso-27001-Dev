e834f6914c353fe62a5edaba8c0d58f6cd190ec5
samayaca <samayaca>
2026-05-22 23:14:04 -0500

    fix: ajustar sensity

--- src/Hooks/usePermissions.jsx
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-import { getPermissions } from "../utils/permissions";
-
-export function usePermissions() {
-  const roleId = useSelector((state) => state.login.user?.rol_id);
-
-  return getPermissions(roleId);
-}
